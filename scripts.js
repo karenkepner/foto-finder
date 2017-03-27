1c5a026f70ebd44341367dde9c3e6fabe50fc6b5
Karen Kepner <kkepner14@gmail.com>
2017-03-27 09:00:49 -0600

    Peudocode for js.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 var karen = 'kep';
+
+//on click of the choose file btn/input:
+//  * have user select the file
+//  * collect the title and caption of the file
+//  * append the new 'item-box' with the photo,title,caption
+//  * if they haven't entered a title and caption, ask for it here
+//  * resize the photo so it fits and loads quickly
+
+//where will the photo be from? just the photos directory?
+//allow fotos from anywhere.
